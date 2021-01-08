af8c36b0d344f93403c9ac2448c1c90264cb19c9
LH <lh@applh.com>
2021-01-08 11:29:28 +0100

    ajout entity type et partie admin annonce

---
+++ src/Form/AnnonceType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Annonce;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+// POUR LE CHAMP AVEC ENTITE User
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class AnnonceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('url')
+            ->add('description')
+            ->add('photo')
+            ->add('categorie')
+            ->add('datePublication')
+            ->add('user', EntityType::class, [ "class" => User::class, "choice_label" => "email" ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Annonce::class,
+        ]);
+    }
+}
